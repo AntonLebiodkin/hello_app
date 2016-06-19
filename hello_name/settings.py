b138b53d7a320209d17e482f4cfbc0c3b25ce198
AntonLebiodkin <anton.lebiodkin@gmail.com>
2016-06-19 23:29:11 +0300

    fixed port deployment bug

--- hello_name/settings.py
+++ hello_name/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'hello_name.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}"""
 
 
 # Password validation
@@ -114,10 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join('name_app', 'static')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join('/static/', 'static')
